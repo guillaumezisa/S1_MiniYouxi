f0cb0175d5635e8248ea484cd3c282f3eee4cb18
LEBLANC <LEBLANC@intechS1.fr>
2017-11-20 11:29:31 +0100

    ajouter req_quitter_pfc.js

---
+++ req_quitter_pfc.js
@@ -0,0 +1,22 @@
+// Requete quitter pour le pfc 
+
+"use strict" 
+
+remedial= ("remedial");
+var fs = require("fs");
+var page;
+var marqeur;
+
+var trait = function ( req , res , query) {
+	page = fs.readFileSync("accueil_membre_MiniYouxi.html","utf-8");
+
+	marqueur = {};
+	marqueur.pseudo = query.pseudo;
+	page = page.supplant(marqueur);
+
+	
+	res.writeHead(200,{"Content-Type": "text/html"});
+	res.write(page);
+	res.end();
+};
+
