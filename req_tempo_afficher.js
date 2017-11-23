6c62ba98384f8028a4078a8b66555a6e20df191e
Red <red@localhost.localdomain>
2017-11-23 09:38:59 +0100

    ajout du jeu tempo <3<3<3

---
+++ req_tempo_afficher.js
@@ -0,0 +1,22 @@
+// traitement de /req_tempo_regle
+"use strict";
+
+var fs = require("fs");
+require("remedial")
+var page;
+var marqueur;
+
+var trait = function (req, res, query) {
+    
+	page = fs.readFileSync("tempo_regle.html", "UTF-8");
+
+	marqueur = {}
+	marqueur.pseudo = query.pseudo
+	page = page.supplant(marqueur)
+
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write(page);
+	res.end(); 
+}
+
+module.exports = trait;
