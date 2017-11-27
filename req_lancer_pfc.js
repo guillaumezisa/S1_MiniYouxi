85fa7168cea02178620baa70ba2a9f1c00563370
LUI <LUI@intechS1.fr>
2017-11-27 16:19:21 +0100

    Ajout lancer pfc

---
+++ req_lancer_pfc.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var fs = require("fs");
+require('remedial');
+
+var trait = function (req, res, query) {
+
+	var marqueurs;
+	var page;
+
+	// AFFICHAGE DE LA PAGE D'ACCUEIL
+
+	page = fs.readFileSync('pierre-feuille-ciseaux.html', 'UTF-8');
+
+	marqueurs = {};
+	marqueurs.erreur = "";
+	marqueurs.pseudo = query.pseudo;
+	page = page.supplant(marqueurs);
+
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write(page);
+	res.end();
+};
+//--------------------------------------------------------------------------
+
+module.exports = trait;
