db8bcdcfb79af26032f731fd226d037f1968afda
DE_WILDE <DE_WILDE@intechS1.fr>
2017-11-27 19:48:53 +0100

    bataille finie manque 4 images

--- req_pose_carte_bataille.js
+++ req_pose_carte_bataille.js
@@ -11,18 +11,39 @@ var resultat;
 var page;
 var marqueur = {}
 
+var choix_couleur = function(chaine) {
+    switch (chaine) {
+	    case "t":
+		    return "trefle";
+			break;
+		case "co":
+		    return "coeur";
+			break;
+		case "p":
+		    return "pique";
+		    break;
+		case "ca":
+		    return "carreau";
+			break;
+	}
+}
 
 var trait = function(req, res, query) {
     
 	mains = fs.readFileSync("mains_bataille_" + query.pseudo + ".json", "UTF-8");
 	mains = JSON.parse(mains);
 
+	// On pose les cartes sur la table (litéralement)
 	pose_joueur = mains.joueur[0];
-mains.joueur.splice(0, 1);
+	mains.joueur.splice(0, 1);
 	pose_ordi = mains.ordi[0];
 	mains.ordi.splice(0, 1);
 
-	if (pose_joueur[0] > pose_ordi[0]) {
+	if (pose_joueur[0] === pose_ordi[0]) {
+	    resultat = "Égalité";
+		mains.joueur.push(pose_joueur);
+		mains.ordi.push(pose_ordi);
+	} else if (pose_joueur[0] > pose_ordi[0]) {
 	    resultat = "Le joueur l'emporte."
 		mains.joueur.push(pose_joueur, pose_ordi);
 	} else {
@@ -31,16 +52,39 @@ mains.joueur.splice(0, 1);
 	}
     
 	
+	console.log(mains.joueur.length, mains.ordi.length)
 	if (mains.joueur.length === 0 || mains.ordi.length == 0) {
+	// Si un des deux joueurs a perdu
 
-    // faire la condition de fin de partie et faire le bouton abandoner et un
-	// bouton reultat en cours de partie
+	    // On définis les marqueurs
+	    marqueur.pseudo = query.pseudo
+	    marqueur.joueur = mains.joueur.length
+        marqueur.ordi = mains.ordi.length
+
+		if (mains.joueur.length === mains.ordi.length) {
+		    marqueur.winer = "Egalité, vous avez le même nombre de carte"
+        } else if (mains.joueur.length > mains.ordi.length) {
+		    marqueur.winer = "le gagnant est " + query.pseudo
+		} else if (mains.joueur.length < mains.ordi.length) {
+		    marqueur.winer = "le gagnant est ordi"
+		}
+		
+		// On va renvoyé la page
+		page = fs.readFileSync("resultat_bataille.html", "UTF-8");
+	    page = page.supplant(marqueur);
+		     
+		res.writeHead(200, {"Content-Type": "text/html"});
+		res.write(page);
+		res.end();
 
 	} else {
 	    page = fs.readFileSync("resultat_tour_bataille.html", "UTF-8")
 
-		marqueur.ordi = String(pose_ordi[0]) + pose_ordi[1];
-		marqueur.joueur = String(pose_joueur[0]) + pose_joueur[1];
+		marqueur.nb_ordi = String(pose_ordi[0]);
+		marqueur.couleur_ordi = choix_couleur(pose_ordi[1]);
+		marqueur.nb_joueur = String(pose_joueur[0]);
+		marqueur.couleur_joueur = choix_couleur(pose_joueur[1]);
+
 		marqueur.resultat = resultat;
 		marqueur.pseudo = query.pseudo
 
