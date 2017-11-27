32a53741d84ca5823d9083a878344b1532f72b10
DE_WILDE <DE_WILDE@intechS1.fr>
2017-11-27 15:07:34 +0100

    bataille fonctionelle

--- req_quitter_bataille.js
+++ req_quitter_bataille.js
@@ -1,11 +1,11 @@
-// traitement de la requete /req_abandonner_bataille
+// traitement de /req_quitter_bataille
 
 "use strict";
 
 var fs = require("fs");
 require("remedial");
-var marqueur = {};
 var page;
+var marqueur = {};
 
 var trait = function(req, res, query) {
     
@@ -13,12 +13,11 @@ var trait = function(req, res, query) {
 
 	page = fs.readFileSync("accueil_membre_MiniYouxi.html", "UTF-8");
 
-	marqueur.pseudo = query.pseudo
-
-	page = page.supplant(marqueur);
+    marqueur.pseudo = query.pseudo
+    page = page.supplant(marqueur);
 
 	res.writeHead(200, {"Content-Type": "text/html"});
-	res.write(page)
+	res.write(page);
 	res.end();
 
 };
