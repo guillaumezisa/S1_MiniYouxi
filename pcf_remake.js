4d3edbc8fd452f50df9f72b52174eebec8c56205
DE_WILDE <DE_WILDE@intechS1.fr>
2017-11-29 16:34:03 +0100

    insert message here

--- pcf_remake.js
+++ pcf_remake.js
@@ -10,13 +10,16 @@ var jeu = function () {
 
 	liste_image = ["main_0.png", "main_1.png", "main_2.png", "main_3.png", "main_4.png", "main_5.png", "main_6.png"]
 	
+	body = document.querySelector("body");
 
 	for (var idx = 0; idx < liste_image.length; idx += 1) {
-		body = document.querySelector("body");
 		image = document.createElement("img");
 		image.setAttribute("src", liste_image[idx]);
 		body.appendChild(image)
+		image.addEventListener("click", inserer une fonction);
 	}
+
+    
 
 
 
