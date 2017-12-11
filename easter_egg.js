b8d0cfcdad6539bc969c9b6111e00f8e9b4e8398
DE_WILDE <DE_WILDE@intechS1.fr>
2017-12-11 16:10:46 +0100

    message de commit

--- easter_egg.js
+++ easter_egg.js
@@ -5,6 +5,7 @@
 var trait = function (req, res, query) {
     res.writeHead(200, {"Content-Type": "text/html"});
 	res.write("<img src=\"./beautiful-easter-egg.png\">");
+	// What a beautyfull easter egg
 	res.end();
 }
 module.exports = trait;
