405ad7d6fbd49613ab80db98c4977911e0e50b2b
LUI <LUI@intechS1.fr>
2017-12-17 22:49:57 +0100

    je met sa la tranquillou

--- req_regle_pfc.js
+++ req_regle_pfc.js
@@ -1,24 +1,26 @@
-// traitement de /req_regle_pfc
+//regle du pfc
 
-"use strict" ;
 
-var fs= require("fs");
+
+"use strict"
+
+var fs = require("fs");
 require ("remedial")
 var page;
 var marqueur;
 
 var trait = function (req, res, query) {
 
-	page = fs.readFileSync("afficher_regle_pfc.html", "UTF-8")
+	page = fs.readFileSync("afficher_regle_pfc.html", "UTF-8");
 
 	marqueur = {}
 	marqueur.pseudo = query.pseudo
 	page = page.supplant(marqueur)
 
-	res.writeHead(200, {'Content-Type' : 'text/html'});
+	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.write(page);
 	res.end();
 
 }
-module.exports = trait;
 
+module.exports = trait;
