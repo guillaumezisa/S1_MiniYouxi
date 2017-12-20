a37613f15fb6b7dc3563e611e2dc6f1592dd3ffa
LUI <LUI@intechS1.fr>
2017-12-20 09:49:33 +0100

    lagny

--- req_jouer_mental.js
+++ req_jouer_mental.js
@@ -10,22 +10,22 @@ require ('remedial') ;
 var  trait = function (request, response, query) {
 
 	var page;
-	var marqueur = {};
+	var marqueur = {} ;
 	var comparaison;
 	var nb_A;
 	var nb_B;
 	var reponse;
 	var resultat;
 	var no;
+	var operateur;
 
 	nb_A = Math.floor(Math.random()*10)+1
-	nb_B = Math.floor(Math.random()*10)+1
+		nb_B = Math.floor(Math.random()*10)+1
 
 	marqueur.nb_A = nb_A;
 	marqueur.nb_B = nb_B;
 
-    no = Math.floor(Math.random()*5);
-	console.log(nb);
+	no = Math.floor(Math.random()*5);
 	switch (no) {
 		case 0:
 			operateur = "+";
@@ -35,7 +35,7 @@ var  trait = function (request, response, query) {
 			operateur = "*";
 			resultat = nb_A * nb_B;
 			break;
-    	case 2 :
+		case 2 :
 			operateur = "-";
 			resultat = nb_A - nb_B;
 			break;
@@ -45,5 +45,16 @@ var  trait = function (request, response, query) {
 			break;
 	}
 	marqueur.operateur = operateur;
+	
+	page = fs.readFileSync("mental.html" , "UTF-8");
+	page = page.supplant(marqueur);
 
-}
+	response.writeHead(200,{"Content-Type" : " text/html"});
+	response.write(page);
+	response.end();
+
+
+
+}; 
+
+module.exports = trait ; 
