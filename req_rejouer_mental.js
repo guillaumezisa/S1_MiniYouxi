a58b8ba1048e54d74fbd0f6cd090633b88279c85
LUI <LUI@intechS1.fr>
2017-12-20 19:26:25 +0100

    MAJ

---
+++ req_rejouer_mental.js
@@ -0,0 +1,25 @@
+"use strict"
+
+
+var fs = require ("fs");
+require ("remedial");
+var page;
+var marqueur;
+
+
+var trait = function (req, res, query) {
+
+	page = fs.readFileSync("mental.html", "UTF-8");
+
+	marqueur = {}
+	marqueur.pseudo = query.pseudo
+		page = page.supplant(marqueur)
+
+		res.writeHead(200, {'Content-Type' : 'text/html'});
+	res.write(page);
+	res.end();
+
+
+}
+
+module.exports = trait;                         
