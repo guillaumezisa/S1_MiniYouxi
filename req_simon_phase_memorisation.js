ffd02a0ac914c7a2adf71b9559fc4c1a28d41c12
Red <red@localhost.localdomain>
2017-12-22 22:09:06 +0100

    <3

--- req_simon_phase_memorisation.js
+++ req_simon_phase_memorisation.js
@@ -10,31 +10,47 @@ var marqueur;
 var trait = function (req, res, query) {
 	var partie ;		// NOM DU JSON DECOMPRESSER
 	var couleur ;		// NUMERO D'UN COULEUR ALEATOIRE
+	var tour ;			// NOMBRE DE TOURS
+	var image ;			// LISTE RECONVERTIE EN STRING DE BOUTON
+	var i;				
 
 	// CHARGEMENT DU JSON
    
 	partie = fs.readFileSync("simon_partie_"+query.pseudo+".json");
 	partie = JSON.parse( partie );
 	
-	// NOUVEAU TOUR DE JEU
-	
-	partie[0] = [Number(partie[0]+1)];
 	
 	// GENERATION D'UNE COULEUR ALEATOIRE
 	
+	tour = partie[0];
 	couleur = Math.floor(Math.random()*4+1);	
-	partie[1].push(couleur);
+	partie[1][tour] = couleur;
 
-	if (partie[0] === /// IL FAUT QUE LA LENGHT DEPARTIR1 SOIT = A PARTI 0
+	console.log("TOUR"+tour);
+	console.log("COULEUR"+couleur);
+	console.log("PARTIE"+ partie[1]);
+	console.log("PARTIE DU TOUR "+partie[1][tour]);
 	
+	// DECHARGEMENT DE LA LISTE DANS UNE STRING 
+	
+	for ( i = 0 ; i < tour+1 ; i++ ){
+		console.log("BOUCLE2");
+		if (i === 0){
+			image = partie[1][i];
+		} else {
+			image = image+partie[1][i];
+		}
+	} 
+	 console.log(image);
 	// CREATION DES MARQUEURS
 	
 	marqueur = {}
 	marqueur.pseudo = query.pseudo
-	
+	marqueur.couleur = image ;
 	
 	// ENREGISTREMENT DU JSON
 	
+	partie[0] = [Number(partie[0]+1)];	
 	partie =JSON.stringify(partie);
 	fs.writeFileSync("simon_partie_"+query.pseudo+".json",partie,"UTF-8");
 	
