5e38645232c459be81c96cb93457d1ca655b633c
DE_WILDE <DE_WILDE@intechS1.fr>
2017-12-30 14:53:06 +0100

    modification des images du morpion et supression de qulques trucs inutiles

--- req_joueur_pose_pion_morpion.js
+++ req_joueur_pose_pion_morpion.js
@@ -29,63 +29,63 @@ var trait = function (req, res, query) {
     if ("place00" in query && marqueur.val00 === "libre"){
 	    marqueur.val00 = "joueur"
 		position[0][0] = "joueur"
-		marqueur.case00 = "carre_jaune.jpg"
+		marqueur.case00 = "croix.png"
 		places.splice(places.indexOf("place00"), 1)
 	}
 	
     if ("place10" in query && query.place10 === "libre"){
 	    marqueur.val10 = "joueur"
 		position[0][1] = "joueur"
-		marqueur.case10 = "carre_jaune.jpg"
+		marqueur.case10 = "croix.png"
 		places.splice(places.indexOf("place10"), 1)
 	}
 	
     if ("place20" in query && query.place20 === "libre"){
 	    marqueur.val20 = "joueur"
 		position[0][2] = "joueur"
-		marqueur.case20 = "carre_jaune.jpg"
+		marqueur.case20 = "croix.png"
 		places.splice(places.indexOf("place20"), 1)
 	}
 
     if ("place01" in query && query.place01 === "libre"){
 	    marqueur.val01 = "joueur"
 		position[1][0] = "joueur"
-		marqueur.case01 = "carre_jaune.jpg"
+		marqueur.case01 = "croix.png"
 		places.splice(places.indexOf("place01"), 1)
 	}
 	
     if ("place11" in query && query.place11 === "libre"){
 	    marqueur.val11 = "joueur"
 		position[1][1] = "joueur"
-		marqueur.case11 = "carre_jaune.jpg"
+		marqueur.case11 = "croix.png"
 		places.splice(places.indexOf("place11"), 1)
 	}
 	    
     if ("place21" in query && query.place21 === "libre"){
 	    marqueur.val21 = "joueur"
 		position[1][2] = "joueur"
-		marqueur.case21 = "carre_jaune.jpg"
+		marqueur.case21 = "croix.png"
 		places.splice(places.indexOf("place21"), 1)
 	}
 
     if ("place02" in query && query.place02 === "libre"){
 	    marqueur.val02 = "joueur"
 		position[2][0] = "joueur"
-		marqueur.case02 = "carre_jaune.jpg"
+		marqueur.case02 = "croix.png"
 		places.splice(places.indexOf("place02"), 1)
 	}
 	    
     if ("place12" in query && query.place12 === "libre"){
 	    marqueur.val12 = "joueur"
 		position[2][1] = "joueur"
-		marqueur.case12 = "carre_jaune.jpg"
+		marqueur.case12 = "croix.png"
 		places.splice(places.indexOf("place12"), 1)
 	}
 
     if ("place22" in query && query.place22 === "libre"){
 	    marqueur.val22 = "joueur"
 		position[2][2] = "joueur"
-		marqueur.case22 = "carre_jaune.jpg"
+		marqueur.case22 = "croix.png"
 		places.splice(places.indexOf("place22"), 1)
 	}
 
@@ -178,55 +178,55 @@ var trait = function (req, res, query) {
 			case "place00":
 				marqueur.val00 = "ordi";
 				position[0][0] = "ordi"
-				marqueur.case00 = "carre_bleu.png";
+				marqueur.case00 = "cercle.png";
 				places.splice(places.indexOf("place00"), 1)
 				break;
 			case "place10":
 				marqueur.val10 = "ordi";
 				position[0][1] = "ordi"
-				marqueur.case10 = "carre_bleu.png"
+				marqueur.case10 = "cercle.png"
 				places.splice(places.indexOf("place10"), 1)
 				break;
 			case "place20":
 				marqueur.val20 = "ordi";
 				position[0][2] = "ordi"
-				marqueur.case20 = "carre_bleu.png"
+				marqueur.case20 = "cercle.png"
 				places.splice(places.indexOf("place20"), 1)
 				break;
 			case "place01":
 				marqueur.val01 = "ordi";
 				position[1][0] = "ordi"
-				marqueur.case01 = "carre_bleu.png"
+				marqueur.case01 = "cercle.png"
 				places.splice(places.indexOf("place01"), 1)
 				break;
 			case "place11":
 				marqueur.val11 = "ordi";
 				position[1][1] = "ordi"
-				marqueur.case11 = "carre_bleu.png"
+				marqueur.case11 = "cercle.png"
 				places.splice(places.indexOf("place11"), 1)
 				break;
 			case "place21":
 				marqueur.val21 = "ordi"
 				position[1][2] = "ordi"
-				marqueur.case21 = "carre_bleu.png"
+				marqueur.case21 = "cercle.png"
 				places.splice(places.indexOf("place21"), 1)
 				break;
 			case "place02":
 				marqueur.val02 = "ordi";
 				position[2][0] = "ordi"
-				marqueur.case02 = "carre_bleu.png";
+				marqueur.case02 = "cercle.png";
 				places.splice(places.indexOf("place02"), 1)
 				break;
 			case "place12":
 				marqueur.val12 = "ordi";
 				position[2][1] = "ordi"
-				marqueur.case12 = "carre_bleu.png";
+				marqueur.case12 = "cercle.png";
 				places.splice(places.indexOf("place12"), 1)
 				break;
 			case "place22":
 				marqueur.val = "ordi";
 				position[2][2] = "ordi"
-				marqueur.case22 = "carre_bleu.png";
+				marqueur.case22 = "cercle.png";
 				places.splice(places.indexOf("place22"), 1)
 				break;
 		}
