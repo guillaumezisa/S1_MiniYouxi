9b3595f873441d42b47e9c77c37d1aa889d89b43
LUI <LUI@intechS1.fr>
2018-01-16 16:56:22 +0100

    aller tere

--- req_abandonner_mental.js
+++ req_abandonner_mental.js
@@ -7,7 +7,7 @@ var page;
 var marqueur;
 
 var trait = function (req, res, query) {
-
+	fs.unlinkSync("jouer_mental_"+ query.pseudo + ".json")
 	page = fs.readFileSync("accueil_membre_MiniYouxi.html", "UTF-8");
 
 	marqueur = {}
